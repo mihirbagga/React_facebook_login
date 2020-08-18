da5978e8e74bd9b6d03a2d7198938c0967a25c2f
MIhir Bagga <62807580+mihirbagga@users.noreply.github.com>
2020-08-18 20:59:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import Facebook from './components/Facebook'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{color:'blue'}}>FACEBOOK LOGIN</h1>
+      
+      <Facebook/>
+    </div>
+  );
+}
+
+export default App;
